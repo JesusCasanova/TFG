1671b4c0b1b4b447999c29e85505a0e60d009620
JesusCasanova <jesucasa@ucm.es>
2024-05-29 22:24:24 +0200

    updates

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -94,11 +94,19 @@ export const login = async (req, res) => {
 
     const token = generateJWT(findUser.id);
 
+    // res.cookie("access_token", token, {
+    //   expires: new Date(Date.now() + 24 * 3600000),
+    //   sameSite: "none",
+    //   secure: true,
+    //   httpOnly: true,
+    // });
+
     res.cookie("access_token", token, {
-      expires: new Date(Date.now() + 24 * 3600000),
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+      partitioned: true,
       sameSite: "none",
       secure: true,
-      httpOnly: true,
     });
 
     res.status(200).json({
@@ -115,11 +123,19 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
+    // res.clearCookie("access_token", {
+    //   expires: new Date(Date.now() - 1),
+    //   sameSite: "none",
+    //   secure: true,
+    //   httpOnly: true,
+    // });
+
     res.clearCookie("access_token", {
-      expires: new Date(Date.now() - 1),
+      expires: new Date(0),
+      httpOnly: true,
+      partitioned: true,
       sameSite: "none",
       secure: true,
-      httpOnly: true,
     });
 
     res.status(200).json({ message: "Sesión cerrada correctamente" });
